1235d52ef5c6e1135107dca751d8a0a7c355cee9
nightmare3832 <nightmare3832@yahoo.co.jp>
2017-03-15 20:40:31 +0900

    Fix

--- src/main/java/cn/nukkit/entity/projectile/EntityArrow.java
+++ src/main/java/cn/nukkit/entity/projectile/EntityArrow.java
@@ -103,17 +103,19 @@ public class EntityArrow extends EntityProjectile {
             this.isCritical = false;
         }
         if(this.particleType == 1){
+            NukkitRandom random = new NukkitRandom();
             Vector3 pos = this.add(
                     this.getWidth() / 2 + ((double) NukkitMath.randomRange(random, -100, 100)) / 500,
                     this.getHeight() / 2 + ((double) NukkitMath.randomRange(random, -100, 100)) / 500,
                     this.getWidth() / 2 + ((double) NukkitMath.randomRange(random, -100, 100)) / 500);
             this.level.addParticle(new DustParticle(pos, rgba[0], rgba[1], rgba[2], rgba[3]));
         }else if(this.particleType == 2){
+            NukkitRandom random = new NukkitRandom();
             Vector3 pos = this.add(
                     this.getWidth() / 2 + ((double) NukkitMath.randomRange(random, -100, 100)) / 500,
                     this.getHeight() / 2 + ((double) NukkitMath.randomRange(random, -100, 100)) / 500,
                     this.getWidth() / 2 + ((double) NukkitMath.randomRange(random, -100, 100)) / 500);
-            this.level.addParticle(new SpellParticle(pos, rgba[0], rgba[1], rgba[2], rgba[3]));
+            this.level.addParticle(new SpellParticle(pos, rgba[0], rgba[1], rgba[2]));
         }
 
         if (this.age > 1200) {
