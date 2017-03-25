dc56a088519588f04716653894afe2c683506e56
Rasuku <yukkurirasuku321@gmail.com>
2017-03-25 20:03:36 +0900

    SoundUpdate

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -25,6 +25,7 @@ import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockAir;
 import cn.nukkit.block.BlockDoor;
 import cn.nukkit.block.BlockEnderChest;
+import cn.nukkit.block.BlockNoteblock;
 import cn.nukkit.blockentity.BlockEntity;
 import cn.nukkit.blockentity.BlockEntityItemFrame;
 import cn.nukkit.blockentity.BlockEntitySign;
@@ -164,6 +165,7 @@ import cn.nukkit.network.protocol.PlayerActionPacket;
 import cn.nukkit.network.protocol.ProtocolInfo;
 import cn.nukkit.network.protocol.RemoveBlockPacket;
 import cn.nukkit.network.protocol.RequestChunkRadiusPacket;
+import cn.nukkit.network.protocol.ResourcePackClientResponsePacket;
 import cn.nukkit.network.protocol.ResourcePacksInfoPacket;
 import cn.nukkit.network.protocol.RespawnPacket;
 import cn.nukkit.network.protocol.SetCommandsEnabledPacket;
@@ -186,6 +188,7 @@ import cn.nukkit.permission.PermissionAttachmentInfo;
 import cn.nukkit.plugin.Plugin;
 import cn.nukkit.potion.Effect;
 import cn.nukkit.potion.Potion;
+import cn.nukkit.resourcepacks.ResourcePack;
 import cn.nukkit.timings.Timing;
 import cn.nukkit.timings.Timings;
 import cn.nukkit.utils.Binary;
@@ -1738,29 +1741,6 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         return (dot1 - dot) >= -maxDiff;
     }
 
-    public void onPlayerPreLogin() {
-        //TODO: AUTHENTICATE
-        this.tryAuthenticate();
-    }
-
-    public void tryAuthenticate() {
-        PlayStatusPacket pk = new PlayStatusPacket();
-        pk.status = PlayStatusPacket.LOGIN_SUCCESS;
-        this.dataPacket(pk);
-        this.authenticateCallback(true);
-    }
-
-    public void authenticateCallback(boolean valid) {
-        //TODO add more stuff after authentication is available
-
-        if (!valid) {
-            this.close("", "disconnectionScreen.invalidSession");
-            return;
-        }
-
-        this.processLogin();
-    }
-
     protected void processLogin() {
         if (!this.server.isWhitelisted((this.getName()).toLowerCase())) {
             this.kick(PlayerKickEvent.Reason.NOT_WHITELISTED, "Server is white-listed");
@@ -1883,12 +1863,6 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         }
 
         if (this.isSpectator()) this.keepMovement = true;
-
-        PlayStatusPacket statusPacket = new PlayStatusPacket();
-        statusPacket.status = PlayStatusPacket.LOGIN_SUCCESS;
-        this.dataPacket(statusPacket);
-
-        this.dataPacket(new ResourcePacksInfoPacket());
 
         if (this.spawnPosition == null && this.namedTag.contains("SpawnLevel") && (level = this.server.getLevelByName(this.namedTag.getString("SpawnLevel"))) != null) {
             this.spawnPosition = new Position(this.namedTag.getInt("SpawnX"), this.namedTag.getInt("SpawnY"), this.namedTag.getInt("SpawnZ"), level);
@@ -2084,8 +2058,65 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                         break;
                     }
 
-                    this.onPlayerPreLogin();
+                    PlayStatusPacket statusPacket = new PlayStatusPacket();
+                    statusPacket.status = PlayStatusPacket.LOGIN_SUCCESS;
+                    this.dataPacket(statusPacket);
 
+                    ResourcePacksInfoPacket infoPacket = new ResourcePacksInfoPacket();
+                    infoPacket.resourcePackEntries = this.server.getResourcePackManager().getResourceStack();
+                    infoPacket.mustAccept = this.server.getForceResources();
+                    this.dataPacket(infoPacket);
+
+                    break;
+                case ProtocolInfo.RESOURCE_PACK_CLIENT_RESPONSE_PACKET:
+                    ResourcePackClientResponsePacket responsePacket = (ResourcePackClientResponsePacket) packet;
+                    System.out.println(responsePacket.responseStatus);
+                    switch (responsePacket.responseStatus) {
+                        case ResourcePackClientResponsePacket.STATUS_REFUSED:
+                            this.close("", "disconnectionScreen.noReason");
+                            break;
+                        case ResourcePackClientResponsePacket.STATUS_SEND_PACKS:
+                            for (String id : responsePacket.packIds) {
+                                ResourcePack resourcePack = this.server.getResourcePackManager().getPackById(id);
+                                if (resourcePack == null) {
+                                    this.close("", "disconnectionScreen.resourcePack");
+                                    break;
+                                }
+
+                                ResourcePackDataInfoPacket dataInfoPacket = new ResourcePackDataInfoPacket();
+                                dataInfoPacket.packId = resourcePack.getPackId();
+                                dataInfoPacket.maxChunkSize = 1048576; //megabyte
+                                dataInfoPacket.chunkCount = resourcePack.getPackSize() / dataInfoPacket.maxChunkSize;
+                                dataInfoPacket.compressedPackSize = resourcePack.getPackSize();
+                                dataInfoPacket.sha256 = resourcePack.getSha256();
+                                this.dataPacket(dataInfoPacket);
+                            }
+                            break;
+                        case ResourcePackClientResponsePacket.STATUS_HAVE_ALL_PACKS:
+                            ResourcePackStackPacket stackPacket = new ResourcePackStackPacket();
+                            stackPacket.mustAccept = this.server.getForceResources();
+                            stackPacket.resourcePackStack = this.server.getResourcePackManager().getResourceStack();
+                            this.dataPacket(stackPacket);
+                            break;
+                        case ResourcePackClientResponsePacket.STATUS_COMPLETED:
+                            this.processLogin();
+                            break;
+                    }
+                    break;
+                case ProtocolInfo.RESOURCE_PACK_CHUNK_REQUEST_PACKET:
+                    ResourcePackChunkRequestPacket requestPacket = (ResourcePackChunkRequestPacket) packet;
+                    ResourcePack resourcePack = this.server.getResourcePackManager().getPackById(requestPacket.packId);
+                    if (resourcePack == null) {
+                        this.close("", "disconnectionScreen.resourcePack");
+                        break;
+                    }
+
+                    ResourcePackChunkDataPacket dataPacket = new ResourcePackChunkDataPacket();
+                    dataPacket.packId = resourcePack.getPackId();
+                    dataPacket.chunkIndex = requestPacket.chunkIndex;
+                    dataPacket.data = resourcePack.getPackChunk(1048576 * requestPacket.chunkIndex, 1048576);
+                    dataPacket.progress = 1048576 * requestPacket.chunkIndex;
+                    this.dataPacket(dataPacket);
                     break;
                 case ProtocolInfo.MOVE_PLAYER_PACKET:
                     if (this.teleportPosition != null) {
@@ -2463,6 +2494,10 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                         if (playerInteractEvent.isCancelled()) {
                             this.inventory.sendHeldItem(this);
                             break;
+                        }
+                        if (target.getId() == Block.NOTEBLOCK) {
+                        	((BlockNoteblock)target).emitSound();
+                        	break;
                         }
                         Block block = target.getSide(((PlayerActionPacket) packet).face);
                         if (block.getId() == Block.FIRE) {
@@ -4133,7 +4168,6 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                     CriticalParticle par = new CriticalParticle(new Vector3(this.x + random.nextRange(-15, 15) / 10, this.y + random.nextRange(0, 20) / 10, this.z + random.nextRange(-15, 15) / 10));
                     this.getLevel().addParticle(par);
                 }
-
                 add = true;
             }
             if (add) source.setDamage((float) (source.getDamage() * 1.5));*/
